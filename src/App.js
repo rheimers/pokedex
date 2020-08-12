c6dcc39044dc91e786ec81abac329161c1c3a6eb
rheimers <regineheimers@gmail.com>
2020-08-12 13:55:13 +0200

    Poke Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import List from "./components/List";
 
 function App() {
-  return <div>Pokedex</div>;
+  return (
+    <div className="app">
+      <header>
+        Pokedex <input />
+      </header>
+      <main>
+        <List></List>
+      </main>
+      <footer>Pokeworld</footer>
+    </div>
+  );
 }
 
 export default App;
