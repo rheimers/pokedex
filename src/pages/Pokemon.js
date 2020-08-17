d77fa25c9b1bfdbde127ac0012a528356b5c0d03
rheimers <regineheimers@gmail.com>
2020-08-17 13:45:56 +0200

    Display single pokemon

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { fetchPokemon } from "../api/pokemon";
+import Loading from "../components/Loading";
 
 function Pokemon() {
   const { name } = useParams();
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   //   ùseParams`hook to access the dynmaic pieces of the URL
-  return <div>{name}</div>;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+        const fetchedPokemon = await fetchPokemon(name);
+        setPokemon(fetchedPokemon);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      }
+    }
+    fetchData();
+  }, [name]);
+
+  if (error) {
+    return <div>ERROR!!!</div>;
+  }
+  if (loading || !pokemon) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <div>ID: {pokemon.id}</div>
+      <div>Name: {pokemon.name}</div>
+      <div>
+        IMG: <img src={pokemon.imgSrc} alt={pokemon.name} />
+      </div>
+    </div>
+  );
 }
 
 export default Pokemon;
