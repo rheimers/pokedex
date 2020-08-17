2a74f27be140d21216a0f8e1ccea053fc5410acc
rheimers <regineheimers@gmail.com>
2020-08-17 10:57:27 +0200

    Add Pokeman Item page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import List from "./components/List";
-import ListItem from "./components/ListItem";
-import ListItemText from "./components/ListItemText";
-import ListItemImg from "./components/ListItemImg";
-import fetchPokemons from "./api/pokemon";
-import Loading from "./components/Loading";
-
-function waitFor(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Pokemons from "./pages/Pokemons";
+import Items from "./pages/Items";
 
 function App() {
-  const [pokeDexList, setPokeDexList] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    async function fetchData() {
-      const pokemons = await fetchPokemons();
-      setPokeDexList(pokemons);
-      await waitFor(3000);
-      setIsLoading(false);
-    }
-    fetchData();
-  }, []);
-  return isLoading ? (
-    <Loading>Hallo</Loading>
-  ) : (
-    <div className="app">
-      <header>
-        <h1>Pokedex</h1>
-        <input className="input" placeholder="Search a Poke" />
-      </header>
-      <main className="colorful-border">
-        <List>
-          {pokeDexList?.map((pokemon) => (
-            <ListItem key={pokemon.name} href={pokemon.link}>
-              <ListItemImg
-                src={pokemon.imgSrc}
-                alt={`Picture of ${pokemon.name}`}
-              />
-              <ListItemText
-                primary={pokemon.name}
-                secondary={`#${pokemon.id}`}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </main>
-      <footer>Pokedex</footer>
-    </div>
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/pokemons">
+            <Pokemons />
+          </Route>
+          <Route path="/items">
+            <Items />
+          </Route>
+          <Route path="/">
+            <Redirect to="/pokemons" />
+          </Route>
+        </Switch>
+        <footer>
+          <Link to="/pokemans">Pokemons</Link>
+          <Link to="/items">Items</Link>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
